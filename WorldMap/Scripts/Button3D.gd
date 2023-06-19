1c6d20aea86d449a196065908de09afbc1793eb3
Chris moore <lninten@gmail.com>
2023-06-18 21:03:24 -0400

    Player can move from node to node

--- WorldMap/Scripts/Button3D.gd
+++ WorldMap/Scripts/Button3D.gd
@@ -2,15 +2,18 @@ extends StaticBody3D
 
 @onready var button = $SubViewport/Button
 
-func _ready():
-	button.connect("input_event", _on_Button3D_input_event)
-	
+var traverseLocation = null
 
-func _on_Button3D_input_event(_camera, event, _position, _normal, _shape_idx):
+signal buttonPressed(location)
+
+func _ready():
+	button.connect("input_event", _on_input_event)
+	
+func _on_button_pressed():
+	print(traverseLocation.locationName)
+	buttonPressed.emit(traverseLocation)
+
+func _on_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			button.emit_signal("pressed")
-	pass
-
-func _on_button_pressed():
-	print("pressed")
--- WorldMap/Scripts/Player.gd
+++ WorldMap/Scripts/Player.gd
@@ -5,25 +5,59 @@ extends CharacterBody3D
 @onready var traverseButtons = $TraverseButtons
 
 var currentLocation = null
+var nextLocation = null
+
+var t = 0.0
+
+func _physics_process(delta):
+	if nextLocation != null:
+		t += delta
+		transform = currentLocation.transform.interpolate_with(nextLocation.transform, t)
+		if transform == nextLocation.transform:
+			currentLocation = nextLocation
+			nextLocation = null
+			create_buttons()
+	else:
+		t = 0.0
+	
+	pass
 
 func start_turn():
 	create_buttons()
 
 #Takes a MapPath node, and moves the player along it
-func traverse(Node):
+func traverse(location : Node):
+	delete_buttons()
+	nextLocation = location
 	pass
 
 #Create a button for each path from the current node
 func create_buttons():
 	if !currentLocation:
 		return
+		
 	for path in currentLocation.paths:
 		var newButton = button.instantiate()
 		traverseButtons.add_child(newButton)
 		newButton.global_position = global_position
-		newButton.global_position.y += 2
-		newButton.look_at(path.to.global_position, Vector3.UP)
-		#newButton.global_position.x += 3
+		newButton.global_position.y += 1.1
+		
+		newButton.global_position.x = (path.to.global_position.x + path.from.global_position.x) / 2
+		newButton.global_position.z = (path.to.global_position.z + path.from.global_position.z) / 2
+		
+		newButton.buttonPressed.connect(traverse)
+		
+		if currentLocation == path.from:
+			newButton.traverseLocation = path.to
+			newButton.look_at(path.to.global_position, Vector3.UP)
+		else:
+			newButton.traverseLocation = path.from
+			newButton.look_at(path.from.global_position, Vector3.UP)
 		newButton.rotation.x = 0
 		newButton.rotation.z = 0
 	pass
+
+func delete_buttons():
+	for child in traverseButtons.get_children():
+		child.queue_free()
+	print(traverseButtons.get_child_count())
